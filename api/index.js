9319ffdb085d2b12019980d8ce64401ca2e6941b
noothi akshith <nakshith_csb223244@mgit.ac.in>
2025-08-10 12:32:19 +0530

    added the index.js

---
+++ api/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+
+import authroutes from "./routes/authroutes.js";
+import userroutes from "./routes/userroutes.js";
+
+const app = express();
+
+// Middleware
+app.use(cors()); // call the function here
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.use(authroutes);
+app.use(userroutes);
+
+// Server
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
