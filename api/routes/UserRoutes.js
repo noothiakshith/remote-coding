d25bca9c2ae52468e133b4d446b728e190cf44a4
noothi akshith <nakshith_csb223244@mgit.ac.in>
2025-08-10 12:36:10 +0530

    added the new todos for the userrotues

--- api/routes/UserRoutes.js
+++ api/routes/UserRoutes.js
@@ -1,4 +1,7 @@
 import express from "express";
 const router = express.Router();
 
+//TODO: get the submission of the problem id
+//TODO: post the submission of the problem
+//TODO:patch the submisssion of the problem which has the testcasesPassed and then stdout and then status
 export default router
