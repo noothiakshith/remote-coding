7bf14f693fbc5bfd4eeecce5144fd5b662da1ddf
noothi akshith <nakshith_csb223244@mgit.ac.in>
2025-08-10 12:57:14 +0530

    added the bqsic userroutes

--- api/routes/UserRoutes.js
+++ api/routes/UserRoutes.js
@@ -1,7 +1,84 @@
 import express from "express";
+import { verifyauth } from "../middlewares/middleware.js";
 const router = express.Router();
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient()
 
-//TODO: get the submission of the problem id
-//TODO: post the submission of the problem
-//TODO:patch the submisssion of the problem which has the testcasesPassed and then stdout and then status
+
+
+router.get('/submission/:id',verifyauth,async(req,res,next)=>{
+    const id = req.params.id;
+    const userid = req?.userid
+    console.log(userid)
+    try{
+        const submission = await prisma.submission.findUnique({
+            where:{
+                problemId:parseInt(id),
+                userId:userid
+            },
+            include:{
+                problem,
+                status,
+                testcasesPassed,
+                runtime,
+            }
+        })
+        return res.status(200).json(submission)
+    }
+    catch(err){
+        console.log(err);
+        return res.status(401).json(err);
+    }
+})
+
+router.post('/submission/:id',verifyauth,async(req,res,next)=>{
+    const id = req.params.id;
+    const userid = req?.userid
+    const{code,language} = req.body
+    try{
+        const newsubmission = await prisma.submission.create({
+            where:{
+                problemId:parseInt(id),
+                userId:userid,
+                source_code:code,
+                language:language
+            }
+        })
+        return res.status(200).json(newsubmission)
+    }
+    catch(err){
+        console.log(err);
+        return res.status(401).json(err);
+    }
+})
+
+
+
+router.patch('/submission/:id',verifyauth,async(req,res,next)=>{
+    const id = req.params.id;
+    const userid = req?.userid;
+    const{testcasesPassed,stdout,status} = req.body
+    try{
+        if(status==="Successful"){
+            //TODO:move to the cleaner worker
+        }
+        const updatesubmission = await prisma.submission.update({
+            where:{
+                id,
+                userId:userid
+            },
+            data:{
+                testCasesPassed:testcasesPassed,
+                stdout,
+                status
+            }
+        })
+    }
+    catch(err){
+        console.log(err);
+        return res.status(401).json(err);
+    }
+})
+
+
 export default router
