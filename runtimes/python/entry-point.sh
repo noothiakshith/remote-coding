7d5a2141484f1ad2dee50dda9b3f7d203d235d78
noothi akshith <nakshith_csb223244@mgit.ac.in>
2025-08-11 21:38:26 +0530

    changed

--- runtimes/python/entry-point.sh
+++ runtimes/python/entry-point.sh
@@ -4,17 +4,16 @@ cd /python
 
 git pull origin main
 
-cp /python/testcases-ee/testcases/$PROBLEM_ID.json ./test_cases.json
+cp /python/testcases/testcases/$PROBLEM_ID.json ./test_cases.json
 
 response=$(curl -s "$CALLBACK_URL/submission/$SUBMISSION_ID")
-
 main_func=$(echo "$response" | jq -r '.source_code')
-
 main_func_name=$(echo "$response" | jq -r '.mainFuncName')
 
 echo "$main_func" > ./main_func.py
-
 export MAIN_FUNC=$main_func_name
+
+
 
 touch std_out.json
 
